0930201b7ad723c8cce1f2322420dda52c124b74
iamayan <ayanh052@gmail.com>
2024-04-25 15:39:21 -0500

    added hashtable

--- src/App.js
+++ src/App.js
@@ -5,11 +5,42 @@ import BranchView from './components/BranchView';
 import './App.css';
 
 function App() {
-
+  
+  // Initialize the dataMap with keys for possible time values
+  const [dataMap, setDataMap] = useState({ '4D': {}, '7D': {}, '10D': {} });
   const [dataUrls, setDataUrls] = useState([]);
 
   const fetchChartData = (filename) => {
     setDataUrls(prevUrls => [...prevUrls, filename]);
+    const parts = filename.split('_');
+    const genotype = parts[0];
+    const treatment = parts[1] === 'H' ? 'HDNT' : 'CONTROL';
+    const time = parts[2]; // e.g., "D4"
+
+    // Check if time key exists in the dataMap, then update the nested structure
+    setDataMap(prevDataMap => {
+      const newDataMap = { ...prevDataMap };
+      
+      // Ensure the time key has an initialized object
+      if (!newDataMap[time]) {
+        newDataMap[time] = {};
+      }
+      
+      // Initialize the genotype key if not already
+      if (!newDataMap[time][genotype]) {
+        newDataMap[time][genotype] = {};
+      }
+
+      // Add or update the treatment data for this genotype
+      newDataMap[time][genotype][treatment] = {
+        link: filename,
+        genotype: genotype,
+        treatment: treatment
+      };
+      console.log(newDataMap);
+      return newDataMap;
+    });
+  
   };
 
   return (
