c2ab9b2bc6fd3dc23f35c59e72a40f11333535ad
Xinyan-Xie <53241048+Xinyan-Xie@users.noreply.github.com>
2024-04-25 17:11:47 -0600

    Add DimensionColorMapping

--- src/components/DimensionSelection.js
+++ src/components/DimensionSelection.js
@@ -1,12 +1,28 @@
 import React from 'react';
 import styles from '../styles/DimensionSelection.module.css';
 import CheckboxTable from './CheckboxTable';
+import { useState } from 'react';
+import DimensionColorMapping from './DimensionColorMapping';
 
 const DimensionSelection = ({ onFetchChartData }) => {
+  
+  const [selectedOptions, setSelectedOptions] = useState({
+    genotype: [],
+    treatment: [],
+    time: []
+  });
+
   return (
-    <div className={styles.dimensionSelection}>
-      <h2 className={styles.dimensionTitle}>Dimension Selection</h2>
-      <CheckboxTable onFetchChartData={onFetchChartData} />
+    <div className={styles.firstColumn}>
+      <div className={styles.dimensionSelection}>
+        <h2 className={styles.dimensionTitle}>
+          Dimension Selection
+        </h2>
+        <CheckboxTable onFetchChartData={onFetchChartData} />
+      </div>
+      <div>
+        <DimensionColorMapping/>
+      </div>
     </div>
   );
 };
